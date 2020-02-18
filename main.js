d4317e1d6ae57684ba998bac33938d825598464e
Daryl McAvoy <dmcavoy631@gmail.com>
2020-02-17 19:38:26 -0600

    basecode and frontend

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+let input = "";
+let pigInput = "";
+
+// GUI SETUP
+//Clear text areas
+const clearTextAreas = () =>{
+  document.getElementById("translateInput").value = "";
+  document.getElementById("translateOutput").value = "";
+}
+
+const translateBtnClick = () =>{
+  pigInput = document.getElementById('translateInput').value;
+  pigLatin(pigInput);
+  
+  // STOPS FORM RELOAD
+  event.preventDefault()
+}
+
+const pigLatin = (input) => {
+
+
+  //Print the latin array as a string without ,'s
+  let result = latinSentence.join(" ");
+  document.getElementById("translateOutput").value = result;
+
+}
+
